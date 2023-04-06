f162084ef5308ab2d0d6007f5048fefbb4998bf4
Yidne <yamiyekn@gmail.com>
2023-04-06 22:01:16 +0300

    Add Book component that is child for Books compnent and create array of objects and pass the array to the book component.

---
+++ src/Components/Book.js
@@ -0,0 +1,64 @@
+import PropTypes from 'prop-types';
+import styles from './modules/Book.module.css';
+
+const Book = ({ books }) => (
+  <div className={styles.container}>
+    <ul className={styles.bookItems}>
+      {
+      books.map((book) => (
+        <li key={book.id} className={styles.bookItem}>
+          <div className={styles.bookContent}>
+            <p className={styles.School}>{book.bookType}</p>
+            <h2 className={styles.Title}>{book.bookTitle}</h2>
+            <p className={styles.SuzanneCollins}>{book.bookAuthor}</p>
+            <ul className={styles.bookEditor}>
+              <li className={styles.Comments}>Comments</li>
+              <li>Remove</li>
+              <li>Edit</li>
+            </ul>
+          </div>
+          <div className={styles.status}>
+            <div className={styles.bookStatus}>
+              <div className={styles.OvalPercentage} />
+              <div className={styles.PercentageWraper}>
+                <p className={styles.Percentage}>{book.percentage}</p>
+                <h3 className={styles.Completed}>Completed</h3>
+              </div>
+            </div>
+            <div className={styles.bookUpdate}>
+              <p className={styles.Current}>CURRENT CHAPTER</p>
+              <h3 className={styles.Chapter}>{book.CurrentChapter}</h3>
+              <button className={styles.button} type="button">UPDATE PROGRESS</button>
+            </div>
+          </div>
+        </li>
+      ))
+      }
+
+    </ul>
+    <hr className={styles.hr} />
+    <form action="" className={styles.form}>
+      <h2 className={styles.formTitle}>ADD NEW BOOK</h2>
+      <div className={styles.formInputs}>
+        <input type="text" name="title" id="bookTitle" className={styles.bookTitle} placeholder="Book title" />
+        <input type="text" name="author" id="bookAuthor" className={styles.bookAuthor} placeholder="Author" />
+        <button type="submit" className={styles.addBook}>ADD BOOK</button>
+      </div>
+    </form>
+  </div>
+);
+
+Book.propTypes = {
+  books: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      percentage: PropTypes.number.isRequired,
+      bookType: PropTypes.string.isRequired,
+      bookTitile: PropTypes.string.isRequired,
+      bookAuthor: PropTypes.string.isRequired,
+      CurrentChapter: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
+
+export default Book;
--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,97 +1,37 @@
-import styles from './modules/Books.module.css';
+import Book from './Book';
 
-const Books = () => (
-  <div className={styles.container}>
-    <ul className={styles.bookItems}>
-      <li className={styles.bookItem}>
-        <div className={styles.bookContent}>
-          <p className={styles.School}>Action</p>
-          <h2 className={styles.Title}>The Hunger Games</h2>
-          <p className={styles.SuzanneCollins}>Suzanne Collins</p>
-          <ul className={styles.bookEditor}>
-            <li className={styles.Comments}>Comments</li>
-            <li>Remove</li>
-            <li>Edit</li>
-          </ul>
-        </div>
-        <div className={styles.status}>
-          <div className={styles.bookStatus}>
-            <div className={styles.OvalPercentage} />
-            <div className={styles.PercentageWraper}>
-              <p className={styles.Percentage}>64%</p>
-              <h3 className={styles.Completed}>Completed</h3>
-            </div>
-          </div>
-          <div className={styles.bookUpdate}>
-            <p className={styles.Current}>CURRENT CHAPTER</p>
-            <h3 className={styles.Chapter}>Chapter 17</h3>
-            <button className={styles.button} type="button">UPDATE PROGRESS</button>
-          </div>
-        </div>
-      </li>
-      <li className={styles.bookItem}>
-        <div className={styles.bookContent}>
-          <p className={styles.School}>Science Fiction</p>
-          <h2 className={styles.Title}>Dune</h2>
-          <p className={styles.SuzanneCollins}>Frank Herbert</p>
-          <ul className={styles.bookEditor}>
-            <li className={styles.Comments}>Comments</li>
-            <li>Remove</li>
-            <li>Edit</li>
-          </ul>
-        </div>
-        <div className={styles.status}>
-          <div className={styles.bookStatus}>
-            <div className={styles.OvalPercentage} />
-            <div className={styles.PercentageWraper}>
-              <p className={styles.Percentage}>8%</p>
-              <h3 className={styles.Completed}>Completed</h3>
-            </div>
-          </div>
-          <div className={styles.bookUpdate}>
-            <p className={styles.Current}>CURRENT CHAPTER</p>
-            <h3 className={styles.Chapter}>Chapter 3: &quot;A Lesson Learned&quot;</h3>
-            <button className={styles.button} type="button">UPDATE PROGRESS</button>
-          </div>
-        </div>
-      </li>
-      <li className={styles.bookItem}>
-        <div className={styles.bookContent}>
-          <p className={styles.School}>Economy</p>
-          <h2 className={styles.Title}>Capital in the Twenty-First Century</h2>
-          <p className={styles.SuzanneCollins}>Suzanne Collins</p>
-          <ul className={styles.bookEditor}>
-            <li className={styles.Comments}>Comments</li>
-            <li>Remove</li>
-            <li>Edit</li>
-          </ul>
-        </div>
-        <div className={styles.status}>
-          <div className={styles.bookStatus}>
-            <div className={styles.OvalPercentage} />
-            <div className={styles.PercentageWraper}>
-              <p className={styles.Percentage}>0%</p>
-              <h3 className={styles.Completed}>Completed</h3>
-            </div>
-          </div>
-          <div className={styles.bookUpdate}>
-            <p className={styles.Current}>CURRENT CHAPTER</p>
-            <h3 className={styles.Chapter}>Introduction</h3>
-            <button className={styles.button} type="button">UPDATE PROGRESS</button>
-          </div>
-        </div>
-      </li>
-    </ul>
-    <hr className={styles.hr} />
-    <form action="" className={styles.form}>
-      <h2 className={styles.formTitle}>ADD NEW BOOK</h2>
-      <div className={styles.formInputs}>
-        <input type="text" name="title" id="bookTitle" className={styles.bookTitle} placeholder="Book title" />
-        <input type="text" name="author" id="bookAuthor" className={styles.bookAuthor} placeholder="Author" />
-        <button type="submit" className={styles.addBook}>ADD BOOK</button>
-      </div>
-    </form>
-  </div>
-);
+const Books = () => {
+  const books = [
+    {
+      bookType: 'Action',
+      bookTitle: 'The Hunger Games',
+      bookAuthor: 'Suzanne Collins',
+      percentage: '64%',
+      CurrentChapter: 'Chapter 17',
+      id: '1',
+    },
+    {
+      bookType: 'Science Fiction',
+      bookTitle: 'Dune',
+      bookAuthor: 'Frank Herbert',
+      percentage: '8%',
+      CurrentChapter: 'Chapter 3: "A Lesson Learned"',
+      id: '2',
+    },
+    {
+      bookType: 'Economy',
+      bookTitle: 'Capital in the Twenty-First Century',
+      bookAuthor: 'Suzanne Collins',
+      percentage: '0%',
+      CurrentChapter: 'Introduction',
+      id: '3',
+    },
+  ];
+  return (
+    <div>
+      <Book books={books} />
+    </div>
+  );
+};
 
 export default Books;
