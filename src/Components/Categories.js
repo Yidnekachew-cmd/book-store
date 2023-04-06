0e12b9ec624d9449e444eb6128fb218ffddac16a
Yidne <yamiyekn@gmail.com>
2023-04-06 18:40:06 +0300

    Change the button name for the categories component.

--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -3,7 +3,7 @@ import styles from './modules/Categories.module.css';
 const Categories = () => (
   <>
     <h1 className={styles.container}>catagories</h1>
-    <button className={styles.button} type="button">Status</button>
+    <button className={styles.button} type="button">Check Status</button>
   </>
 );
 
