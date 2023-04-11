e8600304ca826a6dd49db4d5b0dbff0da5ea63a0
Yidne <yamiyekn@gmail.com>
2023-04-11 11:33:38 +0300

    Create UseStates for the title and author and by using useDispatch, update the state.

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,15 +1,50 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import styles from './modules/Form.module.css';
+import { addBook } from '../redux/Books/booksSlice';
 
-const Form = () => (
-  <div className={styles.container}>
-    <form action="" className={styles.form}>
-      <h2 className={styles.formTitle}>ADD NEW BOOK</h2>
-      <div className={styles.formInputs}>
-        <input type="text" name="title" id="bookTitle" className={styles.bookTitle} placeholder="Book title" />
-        <input type="text" name="author" id="bookAuthor" className={styles.bookAuthor} placeholder="Author" />
-        <button type="submit" className={styles.addBook}>ADD BOOK</button>
-      </div>
-    </form>
-  </div>
-);
+const Form = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (title && author) dispatch(addBook({ item_id: uuidv4(), title, author }));
+    setTitle('');
+    setAuthor('');
+  };
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form} onSubmit={handleSubmit}>
+        <h2 className={styles.formTitle}>ADD NEW BOOK</h2>
+        <div className={styles.formInputs}>
+          <input
+            type="text"
+            name="title"
+            id="bookTitle"
+            className={styles.bookTitle}
+            placeholder="Book title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+          <input
+            type="text"
+            name="author"
+            id="bookAuthor"
+            className={styles.bookAuthor}
+            placeholder="Author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            required
+          />
+          <button type="submit" className={styles.addBook}>ADD BOOK</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
 export default Form;
