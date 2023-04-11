c154e3db7a85624b75bf57b7eb839590a1f24ab7
Yidne <yamiyekn@gmail.com>
2023-04-11 11:31:23 +0300

    update initial state with the given array and modified the reducer.

--- src/redux/Books/booksSlice.js
+++ src/redux/Books/booksSlice.js
@@ -1,20 +1,44 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  books: [],
-};
+const initialState = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+    percentage: '64%',
+    CurrentChapter: 'Chapter 17',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+    percentage: '8%',
+    CurrentChapter: 'Chapter 3: "A Lesson Learned"',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+    percentage: '0%',
+    CurrentChapter: 'Introduction',
+  },
+];
 
 export const booksSlice = createSlice({
   name: 'books',
   initialState,
   reducers: {
     addBook: (state, action) => {
-      state.books.push(action.payload);
+      state.push(action.payload);
     },
-    removeBook: (state, action) => ({
-      ...state,
-      books: state.books.filter((book) => book.id !== action.payload),
-    }),
+    removeBook: (state, action) => {
+      const books = [...state];
+      state.splice(0, state.length);
+      state.push(...books.filter((book) => book.item_id !== action.payload));
+    },
   },
 });
 
